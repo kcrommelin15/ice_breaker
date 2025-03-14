1e3c4b825f5847f8cc4c9c1c2d8599e35ae6f112
Kevin Crommelin <kcrommelin@gmail.com>
2025-03-13 18:32:27 -0700

    Scraping linkedin

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -12,25 +12,32 @@ def scrape_linkedin_profile(linkedin_url: str, mock: bool = False) -> dict:
         linkedin_url (str): The URL of the LinkedIn profile.
         mock (bool, optional): Whether to use mock data. Defaults to False.
     """
-    # if mock:
-    #     return {
-    #         "name": "John Doe",
-    #         "title": "Software Engineer",
-    #         "location": "San Francisco, CA",
-    #         "summary": "I am a software engineer with 5 years of experience.",
-    #     }
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/kcrommelin15/aef7ab6765f7e59e9d986f91c79ad74d/raw/466b38fe2b5333f41088a6e0218297e1788f3b57/kevin-li-scrapin.json"
+        response = requests.get(linkedin_profile_url, timeout=10)
+    else:
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.getenv("SCRAPIN_API_KEY"),
+            "linkedInUrl": linkedin_url,
+        }
+        response = requests.get(api_endpoint, params=params, timeout=10)
+    
+    data = response.json().get("person")
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([],"", "", None)
+        and k not in ["certifications"]
+    }
+    return data
 
-    # headers = {
-    #     "Authorization": f"Bearer {os.getenv('LINKEDIN_TOKEN')}",
-    #     "Accept": "application/json",
-    # }
-    # response = requests.get(linkedin_url, headers=headers)
-    # response.raise_for_status()
-    # data = response.json()
 
-    # return {
-    #     "name": data["localizedFirstName"] + " " + data["localizedLastName"],
-    #     "title": data["localizedHeadline"],
-    #     "location": data["location"]["name"],
-    #     "summary": data["summary"],
-    # }
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            "https://www.linkedin.com/in/kcrommelin/",
+            mock=True
+        )
+    )
